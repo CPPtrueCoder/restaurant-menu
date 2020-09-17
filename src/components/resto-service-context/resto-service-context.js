000929770b35845f13382673b766079e6fb059dd
CPPtrueCoder <andierodin@gmail.com>
2020-09-18 02:08:59 +0300

    Base structure of project realized in file index.js. Add settings for Errors, Context and Routes. Also redux-store, redux-actions had been created

---
+++ src/components/resto-service-context/resto-service-context.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RestoServiceContext =React.createContext();
+
+
+
+export default RestoServiceContext;
---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+const  initialState ={
+    menu:[]
+}
+
+
+
+const reduser = (state=initialState,action)=>{
+
+    switch (action.type){
+        case 'MENU_LOADED':
+            return {
+                menu:action.payload
+            };
+        default :
+            return state;
+
+
+    }
+
+
+}
+
+export default reduser;
